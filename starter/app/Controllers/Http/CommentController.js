becbc57ccf03375ec57b41fa9baab18b5961e94e
Wes <wrbigsky@gmail.com>
2021-03-01 09:35:14 -0700

    add data page

---
+++ starter/app/Controllers/Http/CommentController.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Post = use('App/Models/Post')
+
+class CommentController {
+  async index({ view }) {
+
+    const comments = await Post.all();
+
+
+    return view.render(('database'), {
+     title: 'Latest Comments from controller',
+     posts: comments.toJSON()
+   })
+  }
+}
+
+module.exports = CommentController
