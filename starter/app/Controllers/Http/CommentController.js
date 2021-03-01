568ad94adb6a10bd07153fb6bd9f2bd140121e90
Wes <wrbigsky@gmail.com>
2021-03-01 11:10:16 -0700

    add add

--- starter/app/Controllers/Http/CommentController.js
+++ starter/app/Controllers/Http/CommentController.js
@@ -13,6 +13,29 @@ class CommentController {
      posts: comments.toJSON()
    })
   }
+  async store({ request, response, session }) {
+    // Validate input
+    const validation = await validate(request.all(), {
+      title: 'required|min:3|max:255',
+      body: 'required|min:3'
+    })
+
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
+    const post = new Post()
+
+    post.title = request.input('title')
+    post.body = request.input('body')
+
+    await post.save()
+
+    session.flash({ notification: 'Post Added!' })
+
+    return response.redirect('/database')
+  }
 }
 
 module.exports = CommentController
